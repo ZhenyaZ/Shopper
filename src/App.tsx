1149806eacff192c18d645a01c7cf3d171e45dc4
ZhenyaZ <darkest2004@gmail.com>
2024-08-14 22:58:05 +0200

    Added header and footer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Header/Footer/Footer"
+import Header from "./components/Header/Header"
+
+function App() {
+
+
+  return (
+    <div className="wrapper">
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
